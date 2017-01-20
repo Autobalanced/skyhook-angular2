8887fb4d2bbaa38d1f3571fde386272ee09c6c3f
Scott Connor <scwables@gmail.com>
2017-01-20 15:30:12 +1100

    First commit

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Ng-Bootstrap modules
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, NgbModule.forRoot() ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
