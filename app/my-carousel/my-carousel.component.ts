d24939148b19777b2e239dda8622703c0c413c50
Scott Connor <scwables@gmail.com>
2017-01-24 16:20:27 +1100

    Working image carrousel, needs CSS for transition

--- app/my-carousel/my-carousel.component.ts
+++ app/my-carousel/my-carousel.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 
 @Component({
 	moduleId: module.id,
@@ -10,7 +11,10 @@ import { Component } from '@angular/core';
 export class MyCarouselComponent {
 
   backgroundArray = [
-    'https://s3-ap-southeast-2.amazonaws.com/micepage-public/landing_images/bg2_dark.png'
+    'https://s3-ap-southeast-2.amazonaws.com/micepage-public/landing_images/bg2_dark.png',
+    'https://s3-ap-southeast-2.amazonaws.com/micepage-public/landing_images/bg5_dark.png',
+    'https://s3-ap-southeast-2.amazonaws.com/micepage-public/landing_images/bg3_dark.png',
+    'https://s3-ap-southeast-2.amazonaws.com/micepage-public/landing_images/bg4_dark.png',
   ];
   backgroundCount = 0;
 
@@ -18,4 +22,20 @@ export class MyCarouselComponent {
     'background-image' : 'url(' + this.backgroundArray[this.backgroundCount] + ')',
     'height' : '400px' // Forced height for testing purposes
   };
+
+  ngOnInit() {
+    let interval = Observable.interval(5000)      // Set interval to x milliseconds.
+                             .take(20)            // Hard loop limit for testing (TODO: Remove for production or increase to reasonable limit)
+                             .map((x) => x + 1)   // Set loop index and increment
+                             .subscribe((x) => {  // Observe loop index and execute
+                               if (this.backgroundCount < this.backgroundArray.length - 1) {
+                                 this.backgroundCount++;
+                                 this.customBackground['background-image'] = 'url(' + this.backgroundArray[this.backgroundCount] + ')';
+                               }
+                               else {
+                                 this.backgroundCount = 0;
+                                 this.customBackground['background-image'] = 'url(' + this.backgroundArray[this.backgroundCount] + ')';
+                               }
+                             })
+  }
 }
